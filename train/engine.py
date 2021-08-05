58f0ee92eb7c59d257949dad86a26cb26da13a86
In Kyu  <inqlee0704@gmail.com>
2021-08-05 14:37:43 -0400

    independent from DL_code

---
+++ train/engine.py
@@ -0,0 +1,78 @@
+from tqdm.auto import tqdm
+import torch
+from torch.cuda import amp
+from sklearn import metrics
+import numpy as np
+
+
+def Dice3d(a,b):
+    intersection =  np.sum((a!=0)*(b!=0))
+    volume = np.sum(a!=0) + np.sum(b!=0)
+    if volume == 0:
+        return -1
+    return 2.*float(intersection)/float(volume)
+
+class Segmentor:
+    def __init__(self,model,optimizer,scheduler,loss_fn,device,scaler):
+        self.model = model
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.loss_fn = loss_fn
+        self.device = device
+        self.scaler = scaler
+        self.epoch = 0
+
+    def train(self, data_loader):
+        self.model.train()
+        epoch_loss = 0
+        epoch_dice = 0
+        skip = 0
+        iters = len(data_loader)
+        pbar = tqdm(enumerate(data_loader), total=iters)
+        for step, batch in pbar:
+            self.optimizer.zero_grad()
+            inputs = batch['image'].to(self.device,dtype=torch.float)
+            targets = batch['seg'].to(self.device)
+            with amp.autocast():
+                outputs = self.model(inputs)
+                loss = self.loss_fn(outputs, targets[:, 0, :, :])
+            preds = np.argmax(outputs.cpu().detach().numpy(),axis=1)
+            targets = targets.cpu().detach().numpy()
+            targets = np.squeeze(targets,axis=1)
+            dice = Dice3d(preds,targets)
+            self.scaler.scale(loss).backward()
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
+            self.scheduler.step(self.epoch+step/iters)
+            epoch_loss += loss.item()
+            if dice==-1:
+                skip += 1
+            else:
+                epoch_dice += dice
+            pbar.set_description(f'loss:{loss:.2f}, dice:{dice:.4f}') 
+        return epoch_loss/iters, epoch_dice/(iters-skip)
+
+    def evaluate(self, data_loader):
+        self.model.eval()
+        epoch_loss = 0
+        epoch_dice = 0
+        skip = 0
+        iters = len(data_loader)
+        pbar = tqdm(enumerate(data_loader),total=iters)
+        with torch.no_grad():
+            for step, batch in pbar:
+                inputs = batch['image'].to(self.device,dtype=torch.float)
+                outputs = self.model(inputs)
+                targets = batch['seg'].to(self.device)
+                loss = self.loss_fn(outputs, targets[:, 0, :, :])
+                epoch_loss += loss.item()
+                preds = np.argmax(outputs.cpu().detach().numpy(),axis=1)
+                targets = targets.cpu().detach().numpy()
+                targets = np.squeeze(targets,axis=1)
+                dice = Dice3d(preds,targets)
+                if dice==-1:
+                    skip += 1
+                else:
+                    epoch_dice += dice
+                pbar.set_description(f'loss:{loss:.2f}, dice:{dice:.4f}') 
+            return epoch_loss/iters, epoch_dice/(iters-skip)
---
+++ train/main.py
@@ -0,0 +1,120 @@
+import os
+from dotenv import load_dotenv
+import time
+import random
+
+from UNet import RecursiveUNet
+from engine import Segmentor
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts,CosineAnnealingLR, ReduceLROnPlateau
+from dataloader import prep_dataloader
+
+import numpy as np
+from torch import nn
+from torch.cuda import amp
+import torch
+import SimpleITK as sitk
+sitk.ProcessObject_SetGlobalWarningDisplay(False)
+
+
+def seed_everything(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+
+class CFG:
+    
+    def __init__(self):
+        self.data_path = os.getenv('VIDA_PATH')
+        self.name = "UNet_baseline"
+        self.model = 'RecursiveUNet' # ['UNet', 'RecursiveUNet']
+        self.optimizer = 'adam' # ['adam', 'adamp']
+        self.scheduler = 'CosineAnnealingWarmRestarts' 
+        # ['CosineAnnealingWarmRestarts', 'CosineAnnealingLR', 'ReduceLROnPlateau']
+        self.epochs = 10
+        self.T_0 = self.epochs
+
+        self.in_file = 'ENV18PM_ProjSubjList_cleaned_IN.in'
+        self.lr = 0.0001
+        self.min_lr = 1e-6
+        self.train_bs = 16
+        self.valid_bs = 32
+        self.test_results_dir = "RESULTS"
+        self.mask = 'airway'
+        self.save = False
+        self.debug = True
+
+if __name__ == "__main__": 
+    load_dotenv()
+    seed_everything()
+    start = time.time()
+
+    c = CFG()
+    n_case = 64
+    print('***********************************************************')
+    print('Configuration: ')
+    print(c.__dict__)
+    print('***********************************************************')
+    
+    # Data
+    if c.debug: # only use 10 cases, 1 epoch
+        train_loader, valid_loader = prep_dataloader(c,n_case=10)
+        c.epochs = 1
+    else:
+        train_loader, valid_loader = prep_dataloader(c,n_case=n_case)
+
+    # Model
+    model = RecursiveUNet(activation=nn.ELU(inplace=True))
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(DEVICE)
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(),lr=c.lr)
+    scheduler = CosineAnnealingWarmRestarts(optimizer, 
+                                                T_0=c.T_0, 
+                                                T_mult=1, 
+                                                eta_min=c.min_lr,
+                                                last_epoch=-1)
+
+    scaler = amp.GradScaler()
+    eng = Segmentor(model=model, 
+                 optimizer=optimizer,
+                 scheduler=scheduler,
+                 loss_fn=loss_function,
+                 device=DEVICE,
+                 scaler=scaler)
+
+    if c.save:
+        dirname = f'{c.name}_{time.strftime("%Y%m%d", time.gmtime())}'
+        out_dir = os.path.join('RESULTS',dirname)
+        os.makedirs(out_dir, exist_ok=True)
+        path = os.path.join(out_dir, "airway_UNet.pth")
+
+    best_loss = np.inf
+    best_dice = 0
+    # Train
+    for epoch in range(c.epochs):
+        trn_loss, trn_dice = eng.train(train_loader)
+        val_loss, val_dice = eng.evaluate(valid_loader)
+        if c.scheduler == 'ReduceLROnPlateau':
+            scheduler.step(val_loss)
+        eng.epoch += 1
+        print(f'Epoch: {epoch}, train loss: {trn_loss:5f}, valid loss: {val_loss:5f}')
+        print(f'Epoch: {epoch}, train dice: {trn_dice:5f}, valid dice: {val_dice:5f}')
+       #  if val_dice > best_dice:
+       #      best_dice = val_dice
+       #      print('Best Dice:', best_dice)
+       #      if c.save:
+       #          torch.save(model.state_dict(), path)
+        if val_loss < best_loss:
+            best_loss = val_loss
+            best_dice = val_dice
+            print(f'Best loss: {best_loss} with {best_dice}')
+            if c.save:
+                torch.save(model.state_dict(), path)
+
+    end = time.time()
+    print('Elapsed time: ' + str(end-start))
